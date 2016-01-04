6ba4da9e0a760c820bfcf93095befdfb6603319c
thilko <thilko.richter@googlemail.com>
2016-01-04 22:13:13 +0100

    elixir playground opened

---
+++ lib/elixir_playground.ex
@@ -0,0 +1,11 @@
+defmodule ElixirPlayground do
+
+  def sum(number1, number2) do
+    number1 + number2
+  end
+
+  defp a_private_function do
+    
+  end
+
+end
---
+++ test/elixir_playground_test.exs
@@ -0,0 +1,20 @@
+defmodule ElixirPlaygroundTest do
+  use ExUnit.Case
+  doctest ElixirPlayground
+
+  test "I can create functions" do
+    assert ElixirPlayground.sum(1,2) == 3
+  end
+
+  test "I can store functions in a variable" do
+    a_function = &ElixirPlayground.sum/2
+    assert a_function.(1,2) == 3
+  end
+
+  test "I can use the pipe operator to use the result for the next call" do
+    powie = &:math.pow/2
+    assert ElixirPlayground.sum(3,4) |> powie.(2) == 49 
+  end
+
+
+end
