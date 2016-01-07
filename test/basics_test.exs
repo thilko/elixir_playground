7f8ee9f2e63042789d08b3ba6d7431cae7e1afba
thilko <thilko.richter@googlemail.com>
2016-01-07 13:46:57 +0100

    pattern matching

--- test/basics_test.exs
+++ test/basics_test.exs
@@ -7,4 +7,18 @@ defmodule BasicsTest do
 
     assert elem(car, 1) == :red
   end
+
+  test "Tuples and pattern matching" do
+    assert "Hello Thilko" == pattern_matching({:hello, "Thilko"})
+    assert "Hi Thilko" == pattern_matching({:hi, "Thilko"})
+  end
+
+  def pattern_matching({:hello, name})do
+    "Hello #{name}"
+  end
+
+  def pattern_matching({:hi, name})do
+    "Hi #{name}"
+  end
+
 end
